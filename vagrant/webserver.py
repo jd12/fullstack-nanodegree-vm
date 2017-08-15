8d6420ab751be0323c799cf069eaa2cecdbc8c33
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2017-08-15 07:31:21 -0700

    Attempt to add POST functionality

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
@@ -9,13 +10,46 @@ class WebServerHandler(BaseHTTPRequestHandler):
 			self.send_header('Content-type','text/html')
 			self.end_headers()
 			message = ""
-			message += "<html><body>Hello!</body></html>"
+			message += "<html><body>"
+			message += "<h1>Hello</h1>"
+			message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>''' 
+			message += "</body></html>"
+			self.wfile.write(message)
+			print message
+			return
+		if self.path.endswith("/hola"):
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			message = ""
+			message += "<html><body><h1>&#161Hola!</h1>"
+			message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>''' 
+			message += "</body></html>"
 			self.wfile.write(message)
 			print message
 			return
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
 
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			ctype, pdict = cgi.parseHeader(self.headers.getheader('content-type'))
+			if ctype == 'multipart/form-data':
+				fields = cgi.parse_multipart(self.rfile,pdict)
+				messagecontent = fields.get('message')
+			message = ""
+			message += "<html><body>"
+			message += "<h2>Okay, how about this:</h2>"
+			message += "<h1> %s </h1>" % messagecontent[0]
+			message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>''' 
+			message += "</body></html>"
+			self.wfile.write(message)
+			print message
+		except:
+			pass
 def main():
 	try:
 		port = 8080
