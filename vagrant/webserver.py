4e906e198c72b3b8785555325a06100b767054c8
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2017-09-25 18:06:33 -0700

    Add Create New Restaurant functionality

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,6 +1,16 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#Create session and connect to database
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 class WebServerHandler(BaseHTTPRequestHandler):
 	
@@ -17,37 +27,58 @@ class WebServerHandler(BaseHTTPRequestHandler):
 			self.wfile.write(message)
 			print message
 			return
-		if self.path.endswith("/hola"):
+		if self.path.endswith("/restaurants/new"):
 			self.send_response(200)
 			self.send_header('Content-type','text/html')
 			self.end_headers()
 			message = ""
-			message += "<html><body><h1>&#161Hola!</h1>"
-			message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>''' 
+			message += "<html><body>"
+                        message += "<h1>Make a New Restaurant</h1>"
+			message += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                        message += "<input name='newRestaurantName' type='text' placeholder= 'New Restaurarant Name' >"
+                        message += "<input type='submit' value='Create'> </form>"
 			message += "</body></html>"
 			self.wfile.write(message)
-			print message
+			return
+                if self.path.endswith("/restaurants") or self.path.endswith("/restaurants/"):
+                        restaurants = session.query(Restaurant).all()
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			message = ""
+			message += "<html><body>"
+                        message += "<a href='/restaurants/new'> Make a New Restaurant</a>" 
+			message += "<h1>Restaurants in Database</h1>"
+                        for restaurant in restaurants:
+                            message += restaurant.name
+                            message += "<br>"
+                            message += "<a href='#'>Edit</a><br>"
+                            message += "<a href='#'>Delete</a><br>"
+                            message += "<br><br><br>"
+			message += "</body></html>"
+			self.wfile.write(message)
 			return
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
 
 	def do_POST(self):
 		try:
-			self.send_response(301)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
-			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-			if ctype == 'multipart/form-data':
-				fields = cgi.parse_multipart(self.rfile,pdict)
-				messagecontent = fields.get('message')
-			message = ""
-			message += "<html><body>"
-			message += "<h2>Okay, how about this:</h2>"
-			message += "<h1> %s </h1>" % messagecontent[0]
-			message += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>''' 
-			message += "</body></html>"
-			self.wfile.write(message)
-			print message
+                        if self.path.endswith("/restaurants/new"):
+                            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                            if ctype == 'multipart/form-data':
+                                    fields = cgi.parse_multipart(self.rfile,pdict)
+                                    messagecontent = fields.get('newRestaurantName')
+
+                                    #Create new Restaurant object
+                                    newRestaurant = Restaurant(name=messagecontent[0])
+                                    session.add(newRestaurant)
+                                    session.commit()
+
+                                    #Finish up webpage
+                                    self.send_response(301)
+                                    self.send_header('Content-type', 'text/html')
+                                    self.end_headers()
+                                
 		except:
 			pass
 def main():
